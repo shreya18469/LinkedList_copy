7b08480353ec1fe52e638ddffcf309a150d20930
shreya18469 <84868815+shreya18469@users.noreply.github.com>
2023-01-19 13:16:15 -0800

    Create node.h

---
+++ node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include <cstring>
+#include "student.h"
+using namespace std;
+
+class Node{
+ public:
+  Node(Student* newStu);
+  ~Node();
+  void setStudent(Student* newStu); //set node to student
+  char* getStudent();//return student found in node
+  void setNext(Node* newnext);//set next node
+  Node* getNext();//return next node
+ private:
+  Student* value;
+  Node* next;
+};
+#endif
